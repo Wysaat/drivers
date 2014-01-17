e09d123178959cf73a5d9344b64f82c63f1591d5
root <root@linux2.(none)>
2014-01-17 16:00:33 +0800

    3rd

--- scull/scull.c
+++ scull/scull.c
@@ -68,4 +68,135 @@ int scull_release(struct inode *inode, struct file *filp) {
 	return 0;
 }
 
-int scull_trim()
+int scull_trim(struct scull_dev *dev) {
+	struct scull_qset *next, *dptr;
+	int qset = dev->qset;
+	int i;
+
+	for (dptr = dev->data; dptr; dptr = next) {
+		if (dptr->data) {
+			for (i = 0; i < qset; i++)
+				kfree(dptr->data[i]);
+			kfree(dptr->data);
+			dptr->data = NULL;
+		}
+		next = dptr->next;
+		kfree(dptr);
+	}
+	dev->size = 0;
+	dev->quantum = scull_quantum;
+	dev->qset = scull_qset;
+	dev->data = NULL;
+	return 0;
+}
+
+ssize_t scull_read(struct file *filp, char __user *buf, size_t count, loff_t *f_ops) {
+	struct scull_dev *dev = filp->private_data;
+	struct scull_qset *dptr;
+	int quantum = dev->quantum, qset = dev->qset;
+	int itemsize = quantum * qset;
+	int item, s_pos, q_pos, rest;
+	ssize_t retval = 0;
+
+	// if (down_interruptible(&dev->sem))
+	// 	return -ERESTARTSYS;
+	if (*f_pos >= dev->size)
+		goto out;
+	if (*f_pos + count > dev->size)
+		count = dev->size - *f_pos;
+
+	item = (long)*f_pos / itemsize;
+	rest = (long)*f_pos % itemsize;
+	s_pos = rest / quantum;
+	q_pos = rest % quantum;
+
+	dptr = scul_follow(dev, item);
+
+	if (dptr = NULL || !dptr->data || !dptr->data[s_pos])
+		goto out;
+
+	if (count > quantum - q_pos)
+		count = quantum - q_pos;
+
+	if (copy_to_user(buf, dptr->data[s_pos] + q_pos, count)) {
+		retval = -EFAULT;
+		goto out;
+	}
+	*f_pos += count;
+	retval = count;
+
+  out:
+    // up(&dev->sem);
+    return retval;
+}
+
+ssize_t scull_write(struct file *filp, const char __user *buf, size_t count, loff_t *f_pos) {
+	struct scull_dev *dev = filp->private_data;
+	struct scull_qset *dptr;
+	int quantum = dev->quantum, qset = dev->qset;
+	int itemsize = quantum * qset;
+	int item, s_pos, q_pos, rest;
+	ssize_t retval = -ENOMEM;
+
+	// if (down_interruptible(&dev->sem))
+	// 	return -ERESTARTSYS;
+
+	item = (long)*f_pos / itemsize;
+	rest = (long)*f_pos % itemsize;
+	s_pos = rest / quantum;
+	q_pos = rest % quantum;
+
+	dptr = scull_follow(dev, item);
+	if (dptr == NULL)
+		goto out;
+	if (!dptr->data) {
+		dptr->data = kmalloc(qset * sizeof(char *), GFP_KERNEL);
+		if (!dptr->data)
+			goto out;
+		memset(dptr->data, 0, qset * sizeof(char *));
+	}
+	if (!dptr->data[s_pos]) {
+		dptr->data[s_pos] = kmalloc(quantum, GFP_KERNEL);
+		if (!dptr->data[s-pos])
+			goto out;
+	}
+	if (count > quantum - q_pos)
+		count = quantum - q_pos;
+
+	if (copy_from_user(dptr->data[s_pos]+q_pos, buf, count)) {
+		retval = -EFAULT;
+		goto out;
+	}
+	*f_pos += count;
+	retval = count;
+
+	if (dev->size < *f_pos)
+		dev->size = *f_pos;
+
+  out:
+    // up(&dev->sem);
+    return retval;
+}
+
+struct scull_qset *scull_follow(struct scull_dev *dev, int n) {
+	struct scull_qset *qs = dev->data;
+
+	if (!qs) {
+		qs = dev->data = kmalloc(sizeof(struct scull_qset), GFP_KERNEL);
+		if (qs == NULL)
+			return NULL;
+		memset(qs, 0, sizeof(struct scull_qset));
+	}
+
+	while (n--) {
+		if (!qs->next) {
+			qs->next = kmalloc(sizeof(struct scull_qset), GFP_KERNEL);
+			if (qs->next == NULL)
+				return NULL;
+			memset(qs->next, 0, sizeof(struct scull_qset));
+		}
+		qs = qs->next;
+		continue;
+	}
+	return qs;
+}
